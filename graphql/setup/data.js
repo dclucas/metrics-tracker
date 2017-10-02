aa24a2c966ef4b90fbb54427979d8f8f93954b5c
Diogo Lucas <dlucas@atlassian.com>
2017-10-02 12:35:51 +1100

    wip

--- graphql/setup/data.js
+++ graphql/setup/data.js
@@ -74,41 +74,85 @@ const metrics = {
 
 const valueGenerators = {
     "PERCENTAGE": () => casual.random,
-    "COUNT": () => casual.integer,
-    "INT": () => casual.integer,
-    "FLOAT": () => casual.random,
+    "COUNT": () => casual.integer(0),
+    "INT": () => casual.integer(0),
+    "FLOAT": () => casual.double(0),
     "BOOLEAN": () => casual.coin_flip,
 }
 
-const createMeasurement = (metrics) =>
-    R.merge({ 
-        value: valueGenerators[metrics.valueType]() 
-    });
+const getGoalMetrics = (metrics, subject) => R.find(g => g.metrics._id === metrics._id, R.propOr([], 'goals', subject));
 
-const createAssessments = (subject) => ({
-    _id: `${subject._id}/BUILD-${casual.integer()}`,
+const createMeasurement = (metrics, subject) => {
+    const res = R.merge({ 
+        value: valueGenerators[metrics.valueType]() 
+        }, { metrics } );
+    const goal = getGoalMetrics(metrics, subject);
+    return goal? 
+        R.merge(res, { goal: R.omit('metrics', goal) })
+        : res;
+}
+
+const createAssessments = (subject, tag) => ({
+    _id: `${subject._id}/BUILD-${tag}`,
     //createdOn: casual.date(format = moment.ISO_8601),
     type: "BUILD",
     subjectId: subject._id,
     measurements: R.map(
-        createMeasurement
+        m => createMeasurement(m, subject)
     )(R.values(metrics)),
 });
+
+/*
+# Represents a subject goal for a given metric
+type MetricsGoal {
+    # Which metrics this goal relates to
+    metrics: Metrics!
+    # Desired value for the metrics
+    value: Float
+    # How to evaluate the desired value against the current one
+    matchBy: MatchKind
+    # How important (normalized) this goal is
+    weight: Float
+    # add matchWith <- how to compare the current value: current only (default), previous, previous N, mean, average, max
+    # The subject that owns the goal
+    subject: Subject
+}
+*/
+/*
+enum MatchKind {
+    EQUALS
+    NOT_EQUALS
+    GREATER_THAN
+    LESSER_THAN
+    GREATER_OR_EQUAL
+    LESSER_OR_EQUAL
+}
+*/
+const goals = {
+    standard: [
+        { metrics: metrics.branchCoverage, value: .8,  matchBy: 'GREATER_OR_EQUAL'},
+    ]
+}
 
 const subjects = {
     proj01: {
         _id: "PROJ-01",
         name: casual.title,
         description: casual.description,
-        goals: [],
+        goals: goals.standard,
     },
 }
 
 const assessments = {
-    proj01: createAssessments(subjects.proj01),
+    proj01_171: createAssessments(subjects.proj01, 171),
+    proj01_172: createAssessments(subjects.proj01, 172),
+    proj01_173: createAssessments(subjects.proj01, 173),
+    proj01_174: createAssessments(subjects.proj01, 174),
+    proj01_175: createAssessments(subjects.proj01, 175),
 }
 
 module.exports = {
     subjects: R.values(subjects),
     assessments: R.values(assessments),
+    metrics: R.values(metrics),
 }
